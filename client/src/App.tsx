f3e2ec8fb8546020549e2941db346090e7ad65d3
Sambashivarao <boyinasambashivarao@gmail.com>
2025-02-02 16:40:25 +0530

    frontend and backend setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import { Bounce, ToastContainer } from "react-toastify";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/SignUp/SignUp";
+import Home from "./Pages/Home/Home";
+import NewPost from "./Pages/NewPost/NewPost";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/newpost" element={<NewPost />} />
+          <Route path="/auth/signup" element={<SignUp />} />
+        </Routes>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
+      </>
+    );
 }
 
-export default App
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend URL (React Vite)
+    credentials: true,
+  })
+);
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_DB_URL);
+}
+
+main().then(() => {
+  console.log("Connected to Database");
+});
+
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to SafeVision" });
+})
 
 
 
